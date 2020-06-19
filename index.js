0c101165b7d87c666824cf8e215fe679dfa3671a
MrCoderBoy345 <35894769+MrCoderBoy345@users.noreply.github.com>
2020-06-19 02:38:35 -0400

    Status message

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const token = process.env.token
 const channelId = process.env.channelId.toString()
 const botId = process.env.botId.toString()
 
-client.on("ready", () => console.log(`Logged in as ${client.user.tag}`))
+client.on("ready", () => {
+    client.user.setPresence({activity: {name: "your bad memes since 2020", type:"WATCHING"}})
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
 client.on('message', msg => {
     if (msg.attachments.size > 0 && msg.channel.id === channelId && msg.author.id !== botId) {
