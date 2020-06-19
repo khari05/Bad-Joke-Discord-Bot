37a7f99e56a9c7f565d999cae990b24d618a167d
MrCoderBoy345 <35894769+MrCoderBoy345@users.noreply.github.com>
2020-06-19 00:28:30 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const disc = require("discord.js")
+const client = new disc.Client()
+
+const token = process.env.token
+const channelId = process.env.channelId
+const botId = process.env.botId
+
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}`))
+
+client.on('message', msg => {
+    if (msg.attachments.size > 0 && msg.channel.id === channelId && msg.author.id !== botId) {
+        msg.channel.send({ 
+            files: [{
+                attachment: './bad-joke.png',
+                name: 'bad-joke.png'
+            }]
+        })
+    }
+})
+
+client.login(token)
